c9cfe2c7e661b9f65b3462fb26abb55949427145
Marcin Szczurko <email@email.com>
2024-01-13 16:06:07 +0100

    Add CLI argparse logic

---
+++ tree/cli.py
@@ -0,0 +1,36 @@
+"""Directory Tree CLI module."""
+
+import argparse
+import pathlib
+import sys
+
+from . import __version__
+from .tree import DirectoryTree
+
+
+def main():
+    args = parse_cmd_line_arguments()
+    root_dir = pathlib.Path(args.root_dir)
+    if not root_dir.is_dir():
+        print("The specified root directory doesn't exist")
+        sys.exit()
+    tree = DirectoryTree(root_dir)
+    tree.generate()
+
+
+def parse_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        prog="tree",
+        description="Tree, a directory tree generator",
+        epilog="Good luck using Tree",
+    )
+    parser.version = f"Tree v{__version__}"
+    parser.add_argument("-v", "--version", action="version")
+    parser.add_argument(
+        "root_dir",
+        metavar="ROOT_DIR",
+        nargs="?",
+        default=".",
+        help="Generate a full directory tree starting at ROOT_DIR",
+    )
+    return parser.parse_args()
